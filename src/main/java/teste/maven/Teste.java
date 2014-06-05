855891d235758baf99fb8c37b5593eccd02a3b3a
jlamimoso <jlamimoso@10.0.1.10>
2014-06-05 02:12:57 -0300

    Alteracao numero 1

---
+++ src/main/java/teste/maven/Teste.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teste.maven;
+
+/**
+ *
+ * @author jlamimoso
+ */
+public class Teste {
+    public static void main(String[] args) {
+        System.out.printf("Meu primeiro teste com o Maven\n");
+    }
+    
+}
